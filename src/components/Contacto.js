5600a2ad7ccd85efcd5b27d25b49a9691a8920be
Federico <redolfofederico@gmail.com>
2023-10-19 13:31:34 -0300

    server running

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -34,7 +34,7 @@ const Contacto = () => {
             e.preventDefault();
             setButtonText("Sending...")
             let response = await fetch(
-                "https://portafolio-five-peach.vercel.app",
+                "https://portafolio-five-peach.vercel.app/contact",
                 {
                     method: 'POST',
                     headers:{
